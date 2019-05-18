b31c873c4d898faa8cd56f0f3fa5b798fbb4655d
Andreas Herrmann <andreas.herrmann@tweag.io>
2019-05-18 14:50:57 +0200

    serve task list

--- lib/TodoMvp.hs
+++ lib/TodoMvp.hs
@@ -1,4 +1,7 @@
 {-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DerivingStrategies #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TypeOperators #-}
 
@@ -6,14 +9,36 @@ module TodoMvp
   ( runTodoMvp
   ) where
 
+import Data.Aeson (ToJSON)
 import Data.Text (Text)
+import GHC.Generics (Generic)
 import Network.Wai.Handler.Warp
 import Servant
 
-type TodoMvpApi = "hello" :> Get '[JSON] Text
+data Status =
+    Open
+  | Done
+  deriving (Eq, Generic, Show)
+  deriving anyclass ToJSON
+
+data Task = Task
+  { description :: Text
+  , status :: Status
+  }
+  deriving (Eq, Generic, Show)
+  deriving anyclass ToJSON
+
+exampleTasks :: [Task]
+exampleTasks =
+  [ Task { description = "buy milk", status = Open }
+  , Task { description = "buy cheese", status = Open }
+  , Task { description = "buy stamps", status = Done }
+  ]
+
+type TodoMvpApi = "tasks" :> Get '[JSON] [Task]
 
 todoMvpServer :: Server TodoMvpApi
-todoMvpServer = pure "Hello world!"
+todoMvpServer = pure exampleTasks
 
 todoMvpApi :: Proxy TodoMvpApi
 todoMvpApi = Proxy
